bdb42a497f732a40d04753bb4faf5b517e8ad74e
JustNova-git <black2016white@mail.ru>
2021-08-30 09:21:08 +0500

    потом закончу над удалением

--- js/script.js
+++ js/script.js
@@ -1,22 +1,54 @@
-let textItem = document.getElementById("textItem");
-let addButtonItem = document.getElementById("addItem");
+// Использование LocalStorage в качестве хранилища записей
+// Используется один ключ records со всеми записями в виде одной строки
+// Функция addCheck проверяет отсутсвие пустой строки и создаёт фокус на элемент ввода, после подключает функцию addElement
+// Функция addElement генерирует текст для отправки в HTML и в LocalStorage ячейку records
+// Функция addElement принимает параметр text как текст, которые необходимо вывести в содержимое записи
+// Функция addElement прибавляет сгенерированное значение к значению ключу records
+//
 
-addButtonItem.onclick = function () {
-  let text;
-  textItem.value !== "" ? (text = textItem.value) : false;
-  console.log(text);
-  addElement();
+let textItem = document.getElementById("textItem"); // Поле ввода текста
+let addButtonItem = document.getElementById("addItem"); // Кнопка для добавления
+let records = document.getElementById("records"); // Блок с записями
+
+addButtonItem.onclick = addCheck;
+textItem.onkeypress = function (event) {
+  event.keyCode == 13 ? addCheck() : false;
 };
 
-function addElement() {
-  // Создаём новый элемент div
-  // и добавляем в него немного контента
+function addCheck() {
+  if (textItem.value !== "") {
+    text = textItem.value;
+    addElement(text);
+    ``;
+  } else {
+    textItem.focus();
+  }
+}
 
-  var newDiv = document.createElement("div");
-  newDiv.innerHTML = "<h1>Привет!</h1>";
+// function addElement(text) {
+//   records.insertAdjacentHTML(
+//     "afterbegin",
+//     '<div class="todo__record_item" id="item"> <p class="todo__record_record" id="item-text">' +
+//       text +
+//       '</p> <div class="todo__record_right"> <div class="todo__record_change" id="item-change">&#9998;</div> <div class="todo__record_del" id="item-del">&#10006;</div> </div> </div>'
+//   );
+// }
 
-  // Добавляем только что созданный элемент в дерево DOM
+function addElement(text) {
+  recordsText =
+    '<div class="todo__record_item" id="item"> <p class="todo__record_record" id="item-text">' +
+    text +
+    "</p> </div>";
+  recordItem = records.insertAdjacentHTML("afterbegin", recordsText);
 
-  my_div = document.getElementById("records");
-  document.body.insertBefore(newDiv, my_div);
+  nowItems = localStorage.getItem("records");
+  localStorage.setItem("records", nowItems + recordsText);
+
+  textItem.value = "";
+  text = "";
+}
+
+if (localStorage.getItem("records")) {
+  let localValue = localStorage.getItem("records");
+  records.insertAdjacentHTML("afterbegin", localValue);
 }
